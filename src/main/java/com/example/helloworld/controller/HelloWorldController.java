4fb78162fe39f74bdfc81546a65276b567320c7e
Angad Wagh <waghangad@gmail.com>
2025-04-18 11:13:01 +0530

    checkout

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -10,4 +10,9 @@ public class HelloWorldController {
     public String sendGreetings() {
         return "Hello, World!";
     }
+
+    @GetMapping("/")
+    public String sendGreetings() {
+        return "Path , /hello";
+    }
 }
